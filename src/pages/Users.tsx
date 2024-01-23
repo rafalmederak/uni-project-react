0377397c144846e62831ad182af1d48e663779fb
Dawid Migdał <dejf026@gmail.com>
2024-01-23 13:08:43 +0100

    #7 fixed after rewiev

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
-import '../styles/Users.css';
+import 'styles/users.css';
+import { User } from 'interfaces/User';
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  company: string;
-  img: string;
-}
 
 const usersData: User[] = [
-  { id: 1, name: 'User 1', email: 'email1@example.com', company: 'Company 1', img: 'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg' },
-  { id: 2, name: 'User 2', email: 'email2@example.com', company: 'Company 2', img: 'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg' },
-  { id: 3, name: 'User 3', email: 'email3@example.com', company: 'Company 3', img: 'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg' },
-  { id: 4, name: 'User 4', email: 'email4@example.com', company: 'Company 4', img: 'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg' },
-  { id: 5, name: 'User 5', email: 'email5@example.com', company: 'Company 5', img: 'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg' },
-  { id: 6, name: 'User 6', email: 'email6@example.com', company: 'Company 6', img: 'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg' },
+  { id: 1, name: 'User 1', email: 'email1@example.com', company: { name: 'Company 1' } },
+  { id: 2, name: 'User 2', email: 'email2@example.com', company: { name: 'Company 2' } },
+  { id: 3, name: 'User 3', email: 'email3@example.com', company: { name: 'Company 3' } },
+  { id: 4, name: 'User 4', email: 'email4@example.com', company: { name: 'Company 4' } },
+  { id: 5, name: 'User 5', email: 'email5@example.com', company: { name: 'Company 5' } },
+  { id: 6, name: 'User 6', email: 'email6@example.com', company: { name: 'Company 6' } },
 
 ];
 
@@ -27,11 +21,11 @@ const Users = () => {
   const filteredUsers = usersData.filter(user =>
     user.name.toLowerCase().includes(searchTerm) ||
     user.email.toLowerCase().includes(searchTerm) ||
-    user.company.toLowerCase().includes(searchTerm)
+    (user.company.name.toLowerCase().includes(searchTerm))
   );
 
   return (
-    <div className="Users">
+    <div className="users">
       <div className="search-container">
         <input
           type="text"
@@ -44,10 +38,10 @@ const Users = () => {
       <div className="users-container">
         {filteredUsers.map(user => (
           <div className="user-card" key={user.id}>
-            <img src={user.img} alt={`User ${user.id}`} />
+            <img src={'https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg'} alt={`User ${user.id}`} />
             <h2>{user.name}</h2>
             <p>{user.email}</p>
-            <p>{user.company}</p>
+            {<p>{user.company.name}</p>}
           </div>
         ))}
       </div>
