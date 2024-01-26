78cc53fe8f7c1ff5e339024ea752257b8a4ad942
Dawid Migdał <dejf026@gmail.com>
2024-01-26 11:31:45 +0100

    #13 Added Posts section

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from 'react';
+import 'styles/posts.css';
+import { Post } from 'interfaces/Post';
+
+const postsData: Post[] = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+
+  {
+    userId: 2,
+    id: 2,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+
+  {
+    userId: 3,
+    id: 3,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+  
+  {
+    userId: 4,
+    id: 4,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+
+  {
+    userId: 5,
+    id: 5,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+
+  {
+    userId: 6,
+    id: 6,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+  },
+
+];
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const [posts, setPosts] = useState<Post[]>(postsData);
+
+  const handleDeletePost = (postId: number) => {
+    const updatedPosts = posts.filter(post => post.id !== postId);
+    setPosts(updatedPosts);
+  };
+
+
+  return (
+    <div className="posts-container">
+      {postsData.map(post => (
+        <div key={post.id} className="post">
+          <div className="user-info">
+            <img src={`https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg`} alt={`User ${post.userId}'s profile`} />
+            <p>User {post.userId}</p>
+          </div>
+          <h2>{post.title}</h2>
+          <p className="post-text">{post.body}</p>
+          <button onClick={() => handleDeletePost(post.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
