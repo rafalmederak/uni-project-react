c37b5f4c145b5235553fb38396edf0fcc157de58
rafalmederak <rafal.mederak1@gmail.com>
2024-01-27 18:56:31 +0100

    #11 Connect API to the Users and Posts section

--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -1,7 +1,14 @@
+import { User } from "interfaces/User";
+
 export interface Post {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-  }
-  
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface PostsProps {
+  posts: Post[];
+  setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
+  users: User[];
+}
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -6,16 +6,43 @@ import Albums from "pages/Albums";
 import Photos from "pages/Photos";
 import Users from "pages/Users";
 import NotFound from "pages/NotFound";
+import { useEffect, useState } from "react";
+import { User } from "interfaces/User";
+import { Post } from "interfaces/Post";
 
 const MainRoutes = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: User[]) => {
+        setUsers(data);
+      });
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: Post[]) => {
+        setPosts(data);
+      });
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />
+        <Route
+          path="/posts"
+          element={<Posts posts={posts} setPosts={setPosts} users={users} />}
+        />
         <Route path="/albums" element={<Albums />} />
         <Route path="/photos" element={<Photos />} />
-        <Route path="/users" element={<Users />} />
+        <Route path="/users" element={<Users users={users} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
