ebdc1990518171e98b6ad724786321f7a50582e3
rafalmederak <rafal.mederak1@gmail.com>
2024-02-06 19:56:06 +0100

    Modify delete posts permissions

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "styles/comments.css";
-import { Comment, CommentsProps} from "interfaces/Comment";
+import { Comment, CommentsProps } from "interfaces/Comment";
 
 const initialCommentState: Comment = {
   postId: 0,
@@ -10,20 +10,27 @@ const initialCommentState: Comment = {
   body: "",
 };
 
-const Comments = ({ comments, setComments, postId, visible, currentUser }: CommentsProps) => {
-
+const Comments = ({
+  comments,
+  setComments,
+  postId,
+  visible,
+  currentUser,
+}: CommentsProps) => {
   const [newComment, setNewComment] = useState<Comment>(initialCommentState);
 
-  const maxId = Math.max(...comments.map(comment => comment.id));
+  const maxId = Math.max(...comments.map((comment) => comment.id));
 
   useEffect(() => {
-    setNewComment(prevNewComment => ({
+    setNewComment((prevNewComment) => ({
       ...prevNewComment,
-      id: maxId
+      id: maxId,
     }));
   }, [maxId]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setNewComment({
       ...newComment,
       [e.target.name]: e.target.value,
@@ -31,7 +38,12 @@ const Comments = ({ comments, setComments, postId, visible, currentUser }: Comme
   };
 
   const handleAddComment = () => {
-    const newCommentWithPostId = { ...newComment, postId, id: maxId + 1, email: currentUser?.email || "" };
+    const newCommentWithPostId = {
+      ...newComment,
+      postId,
+      id: maxId + 1,
+      email: currentUser?.email || "",
+    };
     if (newComment.name.trim() === "" || newComment.body.trim() === "") {
       alert("Title and body are required!");
       return;
@@ -41,18 +53,22 @@ const Comments = ({ comments, setComments, postId, visible, currentUser }: Comme
   };
 
   const handleDeleteComment = (commentId: number) => {
-    const updatedComments = comments.filter((comment: Comment) => comment.id !== commentId);
+    const updatedComments = comments.filter(
+      (comment: Comment) => comment.id !== commentId
+    );
     setComments(updatedComments);
   };
 
-  const filteredComments = comments.filter(comment => comment.postId === postId);
+  const filteredComments = comments.filter(
+    (comment) => comment.postId === postId
+  );
 
   return (
     <div className={`comments-section ${visible ? "visible" : ""}`}>
       {visible && (
         <div className="comments-container">
           <div className="add-comment">
-          <input
+            <input
               type="text"
               name="name"
               placeholder="Enter comment title"
@@ -67,17 +83,22 @@ const Comments = ({ comments, setComments, postId, visible, currentUser }: Comme
             ></textarea>
             <button onClick={handleAddComment}>Add Comment</button>
           </div>
-          {filteredComments.map((comment:Comment) => (
+          {filteredComments.map((comment: Comment) => (
             <div key={comment.id} className="comment">
               <div className="comment-user-info">
-              <img
-              src={`https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg`}
-              alt={`${comment.email}'s profile`}/> 
-              <p>{comment.email}</p>
-                 </div>
-                 <h3>{comment.name}</h3>
+                <img
+                  src={`https://pic.onlinewebfonts.com/thumbnails/icons_504591.svg`}
+                  alt={`${comment.email}'s profile`}
+                />
+                <p>{comment.email}</p>
+              </div>
+              <h3>{comment.name}</h3>
               <p className="comment-body">{comment.body}</p>
-              <button onClick={() => handleDeleteComment(comment.id)}>Delete Comment</button>
+              {comment.email === currentUser?.email && (
+                <button onClick={() => handleDeleteComment(comment.id)}>
+                  Delete Comment
+                </button>
+              )}
             </div>
           ))}
         </div>
